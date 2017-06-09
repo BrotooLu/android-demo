38508b57b32e9312f620a0916c66f4060226c2d5
Bro2 <liu.bro2@foxmail.com>
2017-06-09 10:10:32 +0800

    modif dbhelper interface

--- b2lib/src/main/java/com/bro2/b2lib/util/DbHelper.java
+++ b2lib/src/main/java/com/bro2/b2lib/util/DbHelper.java
@@ -1,6 +1,8 @@
 package com.bro2.b2lib.util;
 
+import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
 import android.util.Log;
 
 import java.lang.annotation.ElementType;
@@ -9,6 +11,8 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static com.bro2.b2lib.B2LibEnv.DEBUG;
@@ -25,69 +29,77 @@ public class DbHelper {
 
     @Target(ElementType.FIELD)
     @Retention(RetentionPolicy.RUNTIME)
-    public static @interface Column {
+    public @interface Column {
         String name();
+        boolean unique() default false;
     }
 
-    public static <T> List<T> fetchEntities(Class<T> clazz, Cursor cursor) throws Exception {
-        ArrayList<T> list = new ArrayList<>();
-        if (cursor == null) {
-            if (DEBUG) {
-                Log.d(TAG, "[DbHelper.fetchEntities] cursor is null");
-            }
-            return list;
-        }
-
-        int count = cursor.getCount();
-        if (count < 0) {
-            if (DEBUG) {
-                Log.d(TAG, "[DbHelper.fetchEntities] no content");
-            }
-            return list;
-        }
-
+    private static class Statement {
         ArrayList<String> projection = new ArrayList<>();
         ArrayList<Class> projectionType = new ArrayList<>();
         ArrayList<Field> projectionField = new ArrayList<>();
-        for (Field f : clazz.getDeclaredFields()) {
-            Column col = f.getAnnotation(Column.class);
-            if (col != null) {
-                projection.add(col.name());
-                projectionType.add(f.getClass());
-                projectionField.add(f);
-            }
-        }
+    }
 
-        int projectionCount = projection.size();
-        int[] index = new int[projectionCount];
-        for (int i = 0; i < projectionCount; ++i) {
-            index[i] = cursor.getColumnIndex(projection.get(i));
-        }
-
-        for (int i = 0; i < count; ++i) {
-            cursor.moveToNext();
-            T entity = clazz.newInstance();
-            for (int j = 0; j < projectionCount; ++i) {
-                Class c = projectionType.get(j);
-                Field field = projectionField.get(j);
-                boolean accessible = field.isAccessible();
-                if (!accessible) {
-                    field.setAccessible(true);
-                }
-                if (c == boolean.class) {
-                    field.setBoolean(entity, cursor.getInt(index[j]) == 0);
-                } else if (c == int.class) {
-                    field.setInt(entity, cursor.getInt(index[j]));
-                } else {
-                    field.set(entity, cursor.getString(index[j]));
-                }
-                if (!accessible) {
-                    field.setAccessible(false);
+    public static <T> List<T> fetchEntities(Context ctx, Uri uri, String selection,
+                                            String[] selectionArgs, String order,
+                                            Class<T> clazz) {
+        ArrayList<T> list = new ArrayList<>();
+        try {
+            ArrayList<String> projection = new ArrayList<>();
+            ArrayList<Class> projectionType = new ArrayList<>();
+            ArrayList<Field> projectionField = new ArrayList<>();
+            for (Field f : clazz.getDeclaredFields()) {
+                Column col = f.getAnnotation(Column.class);
+                if (col != null) {
+                    projection.add(col.name());
+                    projectionType.add(f.getClass());
+                    projectionField.add(f);
                 }
             }
-            list.add(entity);
-        }
 
+            Cursor cursor = ctx.getContentResolver().query(uri, (String[]) projection.toArray(),
+                    selection, selectionArgs, order);
+            if (cursor == null) {
+                if (DEBUG) {
+                    Log.d(TAG, "[DbHelper.fetchEntities] cursor is null");
+                }
+                return list;
+            }
+
+            int projectionCount = projection.size();
+            int[] index = new int[projectionCount];
+            for (int i = 0; i < projectionCount; ++i) {
+                index[i] = cursor.getColumnIndex(projection.get(i));
+            }
+            int count = cursor.getCount();
+            for (int i = 0; i < count; ++i) {
+                cursor.moveToNext();
+                T entity = clazz.newInstance();
+                for (int j = 0; j < projectionCount; ++i) {
+                    Class c = projectionType.get(j);
+                    Field field = projectionField.get(j);
+                    boolean accessible = field.isAccessible();
+                    if (!accessible) {
+                        field.setAccessible(true);
+                    }
+                    if (c == boolean.class) {
+                        field.setBoolean(entity, cursor.getInt(index[j]) == 0);
+                    } else if (c == int.class) {
+                        field.setInt(entity, cursor.getInt(index[j]));
+                    } else {
+                        field.set(entity, cursor.getString(index[j]));
+                    }
+                    if (!accessible) {
+                        field.setAccessible(false);
+                    }
+                }
+                list.add(entity);
+            }
+        } catch (Exception e) {
+            if (DEBUG) {
+                Log.e(TAG, null, new Exception());
+            }
+        }
         return list;
     }
 
