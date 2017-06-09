ddf7d614a18b0c3c08bb348a8eb80b1d69077791
Bro2 <liu.bro2@foxmail.com>
2017-06-09 15:28:34 +0800

    bug fix

--- b2lib/src/main/java/com/bro2/b2lib/util/DbHelper.java
+++ b2lib/src/main/java/com/bro2/b2lib/util/DbHelper.java
@@ -1,5 +1,6 @@
 package com.bro2.b2lib.util;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
@@ -11,8 +12,6 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import static com.bro2.b2lib.B2LibEnv.DEBUG;
@@ -31,34 +30,147 @@ public class DbHelper {
     @Retention(RetentionPolicy.RUNTIME)
     public @interface Column {
         String name();
-        boolean unique() default false;
+
+        boolean key() default false;
     }
 
     private static class Statement {
         ArrayList<String> projection = new ArrayList<>();
         ArrayList<Class> projectionType = new ArrayList<>();
         ArrayList<Field> projectionField = new ArrayList<>();
+        ArrayList<Field> keyField = new ArrayList<>();
+
+        Statement(Class<?> clazz) {
+            for (Field f : clazz.getDeclaredFields()) {
+                Column col = f.getAnnotation(Column.class);
+                if (col != null) {
+                    projection.add(col.name());
+                    projectionType.add(f.getType());
+                    projectionField.add(f);
+                    if (col.key()) {
+                        keyField.add(f);
+                    }
+                }
+            }
+        }
+
+        String getEntityQuery(Object entity) throws Exception {
+            int keyCount = keyField.size();
+            if (keyCount < 1) {
+                return "1=0";
+            }
+
+            StringBuilder where = new StringBuilder();
+            for (int i = 0; i < keyCount; ++i) {
+                Field field = keyField.get(i);
+                int index = projectionField.indexOf(field);
+                if (i > 0) {
+                    where.append(" AND ");
+                }
+                where.append(projection.get(index));
+                where.append("=");
+                Class type = projectionType.get(index);
+                boolean accessible = field.isAccessible();
+                if (!accessible) {
+                    field.setAccessible(true);
+                }
+                if (type == boolean.class) {
+                    where.append(field.getBoolean(entity));
+                } else if (type == int.class) {
+                    where.append(field.getInt(entity));
+                } else if (type == long.class) {
+                    where.append(field.getLong(entity));
+                } else {
+                    where.append("'");
+                    where.append(field.get(entity));
+                    where.append("'");
+                }
+                if (!accessible) {
+                    field.setAccessible(false);
+                }
+            }
+            return where.toString();
+        }
+
+        ContentValues getContentValues(Object entity) throws Exception {
+            ContentValues values = new ContentValues();
+            int count = projectionField.size();
+            for (int i = 0; i < count; ++i) {
+                Field field = projectionField.get(i);
+                String col = projection.get(i);
+                Class type = field.getType();
+                if (type == boolean.class) {
+                    values.put(col, field.getBoolean(entity));
+                } else if (type == int.class) {
+                    values.put(col, field.getInt(entity));
+                } else if (type == long.class) {
+                    values.put(col, field.getLong(entity));
+                } else {
+                    values.put(col, field.get(entity).toString());
+                }
+            }
+            return values;
+        }
+    }
+
+    public static <T> boolean insertEntity(Context ctx, Uri uri, T entity) {
+        boolean flag = true;
+        try {
+            Statement statement = new Statement(entity.getClass());
+            ctx.getContentResolver().insert(uri, statement.getContentValues(entity));
+        } catch (Exception e) {
+            if (DEBUG) {
+                Log.e(TAG, null, e);
+            }
+            flag = false;
+        }
+        return flag;
+    }
+
+    public static <T> boolean delEntity(Context ctx, Uri uri, T entity) {
+        int count = 0;
+        try {
+            Statement statement = new Statement(entity.getClass());
+            count = ctx.getContentResolver().delete(uri, statement.getEntityQuery(entity), null);
+        } catch (Exception e) {
+            if (DEBUG) {
+                Log.e(TAG, null, e);
+            }
+        }
+        return count > 0;
+    }
+
+    public static <T> boolean updateEntity(Context ctx, Uri uri, T entity) {
+        int count = 0;
+        try {
+            Statement statement = new Statement(entity.getClass());
+            count = ctx.getContentResolver().update(uri, statement.getContentValues(entity),
+                    statement.getEntityQuery(entity), null);
+        } catch (Exception e) {
+            if (DEBUG) {
+                Log.e(TAG, null, e);
+            }
+        }
+        return count > 0;
+    }
+
+    public static <T> List<T> fetchEntities(Context ctx, Uri uri, String selection, Class<T> clazz) {
+        return fetchEntities(ctx, uri, selection, null, null, clazz);
     }
 
     public static <T> List<T> fetchEntities(Context ctx, Uri uri, String selection,
                                             String[] selectionArgs, String order,
                                             Class<T> clazz) {
         ArrayList<T> list = new ArrayList<>();
+        Cursor cursor = null;
         try {
-            ArrayList<String> projection = new ArrayList<>();
-            ArrayList<Class> projectionType = new ArrayList<>();
-            ArrayList<Field> projectionField = new ArrayList<>();
-            for (Field f : clazz.getDeclaredFields()) {
-                Column col = f.getAnnotation(Column.class);
-                if (col != null) {
-                    projection.add(col.name());
-                    projectionType.add(f.getClass());
-                    projectionField.add(f);
-                }
-            }
+            Statement statement = new Statement(clazz);
+            ArrayList<String> projection = statement.projection;
+            ArrayList<Class> projectionType = statement.projectionType;
+            ArrayList<Field> projectionField = statement.projectionField;
+            String[] arr = projection.toArray(new String[projection.size()]);
 
-            Cursor cursor = ctx.getContentResolver().query(uri, (String[]) projection.toArray(),
-                    selection, selectionArgs, order);
+            cursor = ctx.getContentResolver().query(uri, arr, selection, selectionArgs, order);
             if (cursor == null) {
                 if (DEBUG) {
                     Log.d(TAG, "[DbHelper.fetchEntities] cursor is null");
@@ -75,7 +187,7 @@ public class DbHelper {
             for (int i = 0; i < count; ++i) {
                 cursor.moveToNext();
                 T entity = clazz.newInstance();
-                for (int j = 0; j < projectionCount; ++i) {
+                for (int j = 0; j < projectionCount; ++j) {
                     Class c = projectionType.get(j);
                     Field field = projectionField.get(j);
                     boolean accessible = field.isAccessible();
@@ -86,6 +198,8 @@ public class DbHelper {
                         field.setBoolean(entity, cursor.getInt(index[j]) == 0);
                     } else if (c == int.class) {
                         field.setInt(entity, cursor.getInt(index[j]));
+                    } else if (c == long.class) {
+                        field.setLong(entity, cursor.getLong(index[j]));
                     } else {
                         field.set(entity, cursor.getString(index[j]));
                     }
@@ -97,7 +211,11 @@ public class DbHelper {
             }
         } catch (Exception e) {
             if (DEBUG) {
-                Log.e(TAG, null, new Exception());
+                Log.e(TAG, null, e);
+            }
+        } finally {
+            if (cursor != null) {
+                cursor.close();
             }
         }
         return list;
