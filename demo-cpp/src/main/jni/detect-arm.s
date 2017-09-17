979177c52e4131599218421295fa15ebf6700429
Bro2 <liu.bro2@foxmail.com>
2017-09-17 23:25:46 +0800

    detect emulator

--- demo-cpp/src/main/jni/detect-arm.s
+++ demo-cpp/src/main/jni/detect-arm.s
@@ -1,17 +1,17 @@
 .global detectArm
 
 detectArm:
-    PUSH { R4, R5, R6, R7, R8, LR }
+    PUSH { R4, R5, R6, R7, LR }
     MOV R7, #0
-    MOV R8, PC
+    MOV R6, PC
     MOV R4, #0
     ADD R7, R7, #1
-    LDR R5, [R8]
+    LDR R5, [R6]
 TAR:
     ADD R4, R4, #1
-    MOV R8, PC
-    suB R8, R8, #12
-    STR R5, [R8]
+    MOV R6, PC
+    SUB R6, R6, #12
+    STR R5, [R6]
     CMP R4, #2
     BGE EXIT
     CMP R7, #2
@@ -19,4 +19,4 @@ TAR:
     B TAR
 EXIT:
     MOV R0, R4
-    POP { R4, R5, R6, R7, R8, PC }
+    POP { R4, R5, R6, R7, PC }
