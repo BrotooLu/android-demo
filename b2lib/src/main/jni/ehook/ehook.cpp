1caf5e892d3bdf71efc858e69dc87c7325a1ef8d
Brotoo <brotoo-lu@hotmail.com>
2019-05-05 14:59:33 +0800

    mv jni

--- b2lib/src/main/jni/ehook/ehook.cpp
+++
@@ -1,7 +0,0 @@
-#include "ehook.h"
-#include "comm.h"
-
-uintptr_t hook(const char *const lib, const char *const sym, void *fn) {
-
-    return 0;
-}
--- b2lib/src/main/jni/ehook/ehook.h
+++
@@ -1,8 +0,0 @@
-#ifndef EHOOK_EHOOK_H
-#define EHOOK_EHOOK_H
-
-#include <stdint.h>
-
-uintptr_t hook(const char *const lib, const char *const sym, void *fn);
-
-#endif
